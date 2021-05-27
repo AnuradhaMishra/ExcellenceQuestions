5acc1a1cfca5bf07aaa32598e526d1d00c85b1c2
Anuradha Mishra <84852541+AnuradhaMishra@users.noreply.github.com>
2021-05-27 05:41:46 +0530

    Update Q4_SQL.sql

--- Q4_SQL.sql
+++ Q4_SQL.sql
@@ -1,10 +1,2 @@
 create database if not exists excellence ;
 use excellence ;
-
-create table Candidate (Candidate_ID int, CandidateName varchar(50), Email varchar(50)) ;
-
-create table Test_Score (
-				Candidate_ID int, 
-                First_Round int check (First_Round between 0 and 10) ,
-				Second_Round int check (Second_Round between 0 and 10) , 
-                Third_Round int check (Third_Round between 0 and 10)) ;
